90f67babfdc2f544266364597a3ac05d9542993a
matvey2007h-collab <matvey2007h@icloud.com>
2026-04-19 03:08:52 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';   // ← добавьте импорт
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/raisewellness">   {/* ← обёртка с basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
-
